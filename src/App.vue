57420a456d27052e596407b8289f12a6603eb1d6
顾小夕 <2504408987@qq.com>
2023-01-09 11:38:30 +0800

    axios封装|配置路径别名

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import request from "./utils/request";
+const test = async () => {
+  const res = await request.get("/home/index");
+  console.log(res);
+};
+test();
+</script>
 
 <template>app</template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
+import * as path from 'path'
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
