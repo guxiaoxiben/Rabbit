3894dbeac1e98e9dfa53fc1aeb8d34d4f6987b15
顾小夕 <2504408987@qq.com>
2023-01-30 11:59:52 +0800

    实现放大镜效果

--- src/views/goods/components/goods-image.vue
+++ src/views/goods/components/goods-image.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup name="GoodsImage">
 import { propsToAttrMap } from "@vue/shared";
-import { onMounted, onUnmounted, ref } from "vue";
-
+import { computed, onMounted, onUnmounted, ref } from "vue";
+import { useMouseInElement } from "@vueuse/core";
 const props = defineProps<{
   images: string[];
 }>();
@@ -27,12 +27,45 @@ onMounted(() => {
 onUnmounted(() => {
   stop();
 });
+const target = ref(null);
+const { isOutside, elementX, elementY } = useMouseInElement(target);
+const position = computed(() => {
+  let x = elementX.value - 100;
+  let y = elementY.value - 100;
+  if (x <= 0) x = 0;
+  if (x >= 200) x = 200;
+  if (y <= 0) y = 0;
+  if (y >= 200) y = 200;
+  return {
+    x,
+    y,
+  };
+});
 </script>
 <template>
   <div class="goods-image" @mouseenter="stop" @mouseleave="play">
-    <div class="middle">
+    <!-- 定位在右侧的图片 缩略图-->
+    <div
+      class="large"
+      :style="[
+        {
+          backgroundImage: `url(${images[active]})`,
+          backgroundPosition: `${-position.x * 2}px ${-position.y * 2}px`,
+        },
+      ]"
+      v-show="!isOutside"
+    ></div>
+    <!-- 用户hover区域 -->
+    <div ref="target" class="middle">
       <img :src="images[active]" alt="" />
+      <!-- 遮罩 -->
+      <div
+        class="layer"
+        v-show="!isOutside"
+        :style="{ left: position.x + 'px', top: position.y + 'px' }"
+      ></div>
     </div>
+
     <ul class="small">
       <li
         v-for="(item, index) in images"
@@ -52,10 +85,33 @@ onUnmounted(() => {
   height: 400px;
   position: relative;
   display: flex;
+  // 大图片容器的样式
+  z-index: 500;
+  .large {
+    position: absolute;
+    top: 0;
+    left: 412px;
+    width: 400px;
+    height: 400px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    background-repeat: no-repeat;
+    background-size: 800px 800px;
+    background-color: #f8f8f8;
+  }
   .middle {
     width: 400px;
     height: 400px;
-    background: #f5f5f5;
+    // background: #f5f5f5;
+    position: relative;
+    cursor: move;
+    .layer {
+      width: 200px;
+      height: 200px;
+      background: rgba(0, 0, 0, 0.2);
+      left: 0;
+      top: 0;
+      position: absolute;
+    }
   }
   .small {
     width: 80px;
