fbaf62cc56045ca3bcb4869926c77f04c560fc45
顾小夕 <2504408987@qq.com>
2023-02-07 22:31:20 +0800

    抽离并封装效验规则

--- src/views/login/components/login-form.vue
+++ src/views/login/components/login-form.vue
@@ -6,6 +6,13 @@ import userStore from "@/store"
 import { useField, useForm } from "vee-validate"
 import { useIntervalFn } from "@vueuse/core"
 import { useCountDown } from "@/utils/hooks"
+import {
+  accountRule,
+  mobileRule,
+  codeRule,
+  passwordRule,
+  isAgreeRule,
+} from "@/utils/validate"
 
 const type = ref<"account" | "mobile">("account")
 const { user } = userStore()
@@ -30,32 +37,13 @@ const { validate, resetForm } = useForm({
     password: "123456",
     isAgree: true,
   },
-  // 设置效验
+  // 设置校验
   validationSchema: {
-    account(value: string) {
-      if (!value) return "请输入用户名"
-      if (!/^[a-zA-Z]\w{5,19}$/.test(value)) return "字母开头且6-20个字符"
-      return true
-    },
-    password(value: string) {
-      if (!value) return "请输入密码"
-      if (!/^\w{6,12}$/.test(value)) return "密码必须是6-24位字符"
-      return true
-    },
-    isAgree: (value: boolean) => {
-      if (!value) return "请同意隐私条款"
-      return true
-    },
-    mobile: (value: string) => {
-      if (!value) return "请输入手机号"
-      if (!/^1[3-9]\d{9}$/.test(value)) return "手机号格式错误"
-      return true
-    },
-    code: (value: string) => {
-      if (!value) return "请输入验证码"
-      if (!/^\d{6}$/.test(value)) return "验证码格式错误"
-      return true
-    },
+    account: accountRule,
+    mobile: mobileRule,
+    code: codeRule,
+    password: passwordRule,
+    isAgree: isAgreeRule,
   },
 })
 const { value: account, errorMessage: accountError } =
